0eb8160b7479bc1c321dd457b8afcd558b03c542
shing1Sks <shreyashks02@gmail.com>
2024-12-23 22:53:31 +0530

    backend setup

--- app.py
+++ app.py
@@ -66,3 +66,7 @@ def analysis():
 
     prediction = model.predict(input_df)
     return jsonify({"prediction": prediction[0]})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
